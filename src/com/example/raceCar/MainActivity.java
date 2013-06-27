66a5d0ebdc87c2697d4e350b2477260f5f967420
Ciocirdel Georgiana Diana <dianageo93@yahoo.com>
2013-06-27 12:27:42 +0300

    A better way to connect to the car.

--- src/com/example/raceCar/MainActivity.java
+++ src/com/example/raceCar/MainActivity.java
@@ -1,30 +1,118 @@
 package com.example.raceCar;
 
+import java.nio.ByteBuffer;
+
+import com.example.myfirstbluetooth.BluetoothSerialService;
 import com.example.myfirstbluetooth.DeviceListActivity;
 import com.example.myfirstbluetooth.R;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	private BluetoothAdapter mBluetoothAdapter = null;
+	BluetoothSerialService mBtSS = null;
+	public final int HORN_CHANNEL = 134;
+	// Intent request codes
+    private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
+    private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
+    private static final int REQUEST_ENABLE_BT = 3;
+    private static Context context;
+    private static Handler handler = new Handler();
+    private Intent data;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
+		context = this;
+		
+		// Get local Bluetooth adapter
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // If the adapter is null, then Bluetooth is not supported
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		Button pairedDevicesBtn = (Button)findViewById(R.id.bluetooth_connect);
-		final Context context = this;
 		pairedDevicesBtn.setOnClickListener(new Button.OnClickListener() {
 		    public void onClick(View v) {
-		        Intent intent = new Intent(context,DeviceListActivity.class) ;
-		        startActivity(intent);
+		        data = new Intent(context,DeviceListActivity.class) ;
+		        startActivityForResult(data,REQUEST_CONNECT_DEVICE_SECURE);
+		    }
+		});
+		
+		Button goForthBtn = (Button)findViewById(R.id.go_forth);
+		goForthBtn.setOnClickListener(new Button.OnClickListener() {
+		    public void onClick(View v) {
+		        // Check that we're actually connected before trying anything
+		        if (mBtSS.getState() != BluetoothSerialService.STATE_CONNECTED) {
+		            Toast.makeText(context, R.string.not_connected, Toast.LENGTH_SHORT).show();
+		            return;
+		        }
+		        else {
+		        	Toast.makeText(context, "connected" + data.getExtras().getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS), Toast.LENGTH_SHORT).show();
+		        	byte[] send = ByteBuffer.allocate(4).putInt(HORN_CHANNEL).array();
+		        	mBtSS.start();
+		        	mBtSS.write(send);
+		        }
 		    }
 		});
 	}
+	
+	@Override
+    public void onStart() {
+        super.onStart();
+        // If BT is not on, request that it be enabled.
+        // setupChat() will then be called during onActivityResult
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        } else {
+            if (mBtSS == null) 
+            	mBtSS = new BluetoothSerialService(context, handler);
+        }
+    }
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data){
+	    // See which child activity is calling us back.
+	    switch (requestCode) {
+	        case REQUEST_CONNECT_DEVICE_SECURE:
+	            if (resultCode == Activity.RESULT_OK){
+	                connectDevice(data, true);
+	            } 
+	        case REQUEST_ENABLE_BT:
+	            break;
+	    }
+	}
+	
+	private void connectDevice(Intent data, boolean secure) {
+        // Get the device MAC address
+        String address = data.getExtras().getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+        // Get the BluetoothDevice object
+        if(mBluetoothAdapter == null) {
+        	return;
+        }
+        BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+        // Attempt to connect to the device
+        mBtSS.connect(device, secure);
+    }
+
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
