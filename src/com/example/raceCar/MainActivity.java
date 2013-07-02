f6e7442155d5922ac76dc76e07878306ae9ac3e3
Ciocirdel Georgiana Diana <dianageo93@yahoo.com>
2013-07-02 13:06:15 +0300

    Car blinks left.

--- src/com/example/raceCar/MainActivity.java
+++ src/com/example/raceCar/MainActivity.java
@@ -2,10 +2,6 @@ package com.example.raceCar;
 
 import java.nio.ByteBuffer;
 
-import com.example.myfirstbluetooth.BluetoothSerialService;
-import com.example.myfirstbluetooth.DeviceListActivity;
-import com.example.myfirstbluetooth.R;
-
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -13,12 +9,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.example.myfirstbluetooth.BluetoothSerialService;
+import com.example.myfirstbluetooth.DeviceListActivity;
+import com.example.myfirstbluetooth.R;
 
 public class MainActivity extends Activity {
 
@@ -33,10 +32,9 @@ public class MainActivity extends Activity {
     private static Handler handler = new Handler();
     private Intent data;
     private int steer = 0;
-    private int steer_right = 0;
     private int speed = 0;
-    private int speed_front = 0;
     private int lightsCount = 0;
+    private int blinkLeftCount = 0;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -90,123 +88,162 @@ public class MainActivity extends Activity {
 		Button lights = (Button)findViewById(R.id.lights);
 		lights.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-            	byte[] send = ByteBuffer.allocate(4).putInt(codes.LIGHTS_OFF).array();
-            	if(lightsCount == 0) {
-            		send = ByteBuffer.allocate(4).putInt(codes.LIGHTS_SOFT).array();
-            		lightsCount++;
-            	}
-            	else if(lightsCount == 1) {
-            		send = ByteBuffer.allocate(4).putInt(codes.LIGHTS).array();
-            		lightsCount++;
-            	}
+            	if (mBtSS.getState() != BluetoothSerialService.STATE_CONNECTED) {
+					Toast.makeText(context, R.string.not_connected, Toast.LENGTH_SHORT).show();
+				}
             	else {
-            		send = ByteBuffer.allocate(4).putInt(codes.LIGHTS_OFF).array();
-            		lightsCount = 0;
+            		byte[] send = ByteBuffer.allocate(4).putInt(codes.LIGHTS_OFF).array();
+            		if(lightsCount == 0) {
+            			send = ByteBuffer.allocate(4).putInt(codes.LIGHTS_SOFT).array();
+            			lightsCount++;
+            		}
+            		else if(lightsCount == 1) {
+            			send = ByteBuffer.allocate(4).putInt(codes.LIGHTS).array();
+            			lightsCount++;
+            		}
+            		else {
+            			send = ByteBuffer.allocate(4).putInt(codes.LIGHTS_OFF).array();
+            			lightsCount = 0;
+            		}
+            		mBtSS.write(send);
             	}
-				mBtSS.write(send);
             }
         });
 		
-//		Button steerLeftPlus = (Button)findViewById(R.id.steer_left_plus);
-//		steerLeftPlus.setOnTouchListener(new View.OnTouchListener() {
-//			@Override			
-//		    public boolean onTouch(View v, MotionEvent event) {
-//				if(event.getAction() == MotionEvent.ACTION_DOWN) {
-//					byte[] send = ByteBuffer.allocate(4).putInt(codes.STEER_LEFT[steer]).array();
-//					mBtSS.write(send);
-//					if(steer < codes.STEER_LEFT.length - 9) {
-//						steer += 8;
-//					}
-//				}
-//				return true;
-//			}
-//		});
-//		
-//		Button steerLeftMinus = (Button)findViewById(R.id.steer_left_minus);
-//		steerLeftMinus.setOnTouchListener(new View.OnTouchListener() {
-//			@Override			
-//		    public boolean onTouch(View v, MotionEvent event) {
-//				if(event.getAction() == MotionEvent.ACTION_DOWN) {
-//					byte[] send = ByteBuffer.allocate(4).putInt(codes.STEER_LEFT[steer]).array();
-//					mBtSS.write(send);
-//					if(steer > 8) {
-//						steer -= 8;
-//					}
-//				}
-//				return true;
-//			}
-//		});
-//		
-//		Button steerRightPlus = (Button)findViewById(R.id.steer_right_plus);
-//		steerRightPlus.setOnTouchListener(new View.OnTouchListener() {
-//			@Override			
-//		    public boolean onTouch(View v, MotionEvent event) {
-//				if(event.getAction() == MotionEvent.ACTION_DOWN) {
-//					byte[] send = ByteBuffer.allocate(4).putInt(codes.STEER_RIGHT[steer_right]).array();
-//					mBtSS.write(send);
-//					if(steer_right < codes.STEER_RIGHT.length - 9) {
-//						steer_right += 8;
-//					}
-//				}
-//				return true;
-//			}
-//		});
-//		
-//		Button steerRightMinus = (Button)findViewById(R.id.steer_right_minus);
-//		steerRightMinus.setOnTouchListener(new View.OnTouchListener() {
-//			@Override			
-//		    public boolean onTouch(View v, MotionEvent event) {
-//				if(event.getAction() == MotionEvent.ACTION_DOWN) {
-//					byte[] send = ByteBuffer.allocate(4).putInt(codes.STEER_RIGHT[steer_right]).array();
-//					mBtSS.write(send);
-//					if(steer_right > 8) {
-//						steer_right -= 8;
-//					}
-//				}
-//				return true;
-//			}
-//		});
-//		
-//		Button noSteer = (Button)findViewById(R.id.no_steer);
-//		noSteer.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//            	byte[] send = ByteBuffer.allocate(4).putInt(codes.STEER_LEFT[0]).array();
-//            	mBtSS.write(send);
-//            	steer = 0;
-//            	steer_right = 0;
-//            }
-//        });
-//		
-//		Button speedPlus = (Button)findViewById(R.id.speed_plus);
-//		speedPlus.setOnTouchListener(new View.OnTouchListener() {
-//			@Override			
-//		    public boolean onTouch(View v, MotionEvent event) {
-//				if(event.getAction() == MotionEvent.ACTION_DOWN) {
-//					byte[] send = ByteBuffer.allocate(4).putInt(codes.SPEED_BACK[speed]).array();
-//					mBtSS.write(send);
-//					if(speed < codes.SPEED_BACK.length - 9) {
-//						speed += 8;
-//					}
-//				}
-//				return true;
-//			}
-//		});
-//		
-//		Button speedFront = (Button)findViewById(R.id.speed_front);
-//		speedFront.setOnTouchListener(new View.OnTouchListener() {
-//			@Override			
-//		    public boolean onTouch(View v, MotionEvent event) {
-//				if(event.getAction() == MotionEvent.ACTION_DOWN) {
-//					byte[] send = ByteBuffer.allocate(4).putInt(codes.SPEED_FRONT[speed_front]).array();
-//					mBtSS.write(send);
-//					if(speed_front < codes.SPEED_FRONT.length - 9) {
-//						speed_front += 8;
-//					}
-//				}
-//				return true;
-//			}
-//		});
+		speed = codes.SPEED.length/2;
+		Button steerFrButton = (Button)findViewById(R.id.steer_front);
+		steerFrButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	if (mBtSS.getState() != BluetoothSerialService.STATE_CONNECTED) {
+					Toast.makeText(context, R.string.not_connected, Toast.LENGTH_SHORT).show();
+				}
+            	else {
+            		byte[] send = ByteBuffer.allocate(4).putInt(codes.SPEED[speed]).array();
+					mBtSS.write(send);
+					if(speed < codes.SPEED.length - 9) {
+						speed += 8;
+					}
+            	}
+            }
+        });
 		
+		Button steerBackButton = (Button)findViewById(R.id.steer_back);
+		steerBackButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	if (mBtSS.getState() != BluetoothSerialService.STATE_CONNECTED) {
+					Toast.makeText(context, R.string.not_connected, Toast.LENGTH_SHORT).show();
+				}
+            	else {
+            		byte[] send = ByteBuffer.allocate(4).putInt(codes.SPEED[speed]).array();
+					mBtSS.write(send);
+					if(speed > 8) {
+						speed -= 8;
+					}
+            	}
+            }
+        });
+		
+		steer = codes.SPEED.length/2;
+		Button steerLeftButton = (Button)findViewById(R.id.steer_left);
+		steerLeftButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	if (mBtSS.getState() != BluetoothSerialService.STATE_CONNECTED) {
+					Toast.makeText(context, R.string.not_connected, Toast.LENGTH_SHORT).show();
+				}
+            	else {
+            		byte[] send = ByteBuffer.allocate(4).putInt(codes.STEER[steer]).array();
+					mBtSS.write(send);
+					if(steer < codes.STEER.length - 9) {
+						steer += 8;
+					}
+            	}
+            }
+        });
+		
+		Button steerRightButton = (Button)findViewById(R.id.steer_right);
+		steerRightButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	if (mBtSS.getState() != BluetoothSerialService.STATE_CONNECTED) {
+					Toast.makeText(context, R.string.not_connected, Toast.LENGTH_SHORT).show();
+				}
+            	else {
+            		byte[] send = ByteBuffer.allocate(4).putInt(codes.STEER[steer]).array();
+					mBtSS.write(send);
+					if(steer > 8) {
+						steer -= 8;
+					}
+            	}
+            }
+        });
+		
+		Button onoffBtn = (Button)findViewById(R.id.onoff);
+		onoffBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	if (mBtSS.getState() != BluetoothSerialService.STATE_CONNECTED) {
+					Toast.makeText(context, R.string.not_connected, Toast.LENGTH_SHORT).show();
+				}
+            	else {
+            		byte[] send = ByteBuffer.allocate(4).putInt(codes.OFF).array();
+					mBtSS.write(send);
+            	}
+            }
+        });
+		
+		Runnable blinkLeftRunnable = new Runnable() {
+			@Override
+			public void run() {
+				byte[] send = ByteBuffer.allocate(4).putInt(codes.BLINK_LEFT[0]).array();
+				while(true) {
+					if(blinkLeftCount == 1) {
+						mBtSS.write(send);
+						send = ByteBuffer.allocate(4).putInt(codes.BLINK_LEFT[1]).array();
+						mBtSS.write(send);
+						try {
+								Thread.sleep(400);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						send = ByteBuffer.allocate(4).putInt(codes.BLINK_LEFT_OFF).array();
+						mBtSS.write(send);
+						try {
+							Thread.sleep(400);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					else {
+						try {
+							Thread.sleep(200);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		};
+		
+		final Thread blinkRunnableThread = new Thread(blinkLeftRunnable);
+		blinkRunnableThread.start();
+		
+		Button blinkLeft = (Button)findViewById(R.id.blink_left);
+		blinkLeft.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	if (mBtSS.getState() != BluetoothSerialService.STATE_CONNECTED) {
+					Toast.makeText(context, R.string.not_connected, Toast.LENGTH_SHORT).show();
+				}
+            	else {
+            		if(blinkLeftCount == 0) {
+            			blinkLeftCount = 1;
+            		}
+            		else if(blinkLeftCount == 1) {
+            			blinkLeftCount = 0;
+            		}
+            	}
+            }
+        });
 	}
 	
 	@Override
